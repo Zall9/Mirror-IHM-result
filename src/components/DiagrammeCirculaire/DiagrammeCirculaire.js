45eca373465f794e392a3df3dbb6b13e72f805fc
TheSummer1502 <pajeanromain@gmail.com>
2022-04-12 18:37:25 +0200

    feat: Ajout de chart visuel TODO fix erreurs
    Co-authored-by: Paul Delifer <Zall9@users.noreply.github.com>

--- src/components/DiagrammeCirculaire/DiagrammeCirculaire.js
+++ src/components/DiagrammeCirculaire/DiagrammeCirculaire.js
@@ -1,8 +1,11 @@
 import Chart from 'chart.js/auto';
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes, { element } from 'prop-types';
 
 const DiagrammeCirculaire = (props) => {
-  const dataProps = props.data;
+  let myChart;
+  console.log('Props :', props);
+  const dataProps = props.datas;
   const couleurs = props.couleurs;
   const labelsProps = props.labels;
   const titre = props.titre;
@@ -11,7 +14,7 @@ const DiagrammeCirculaire = (props) => {
   const idProps = props.id;
   const booleanIsInteractive = props.booleanIsInteractive;
   const taille = props.taille;
-  const data = {
+  const datas = {
     labels: labelsProps,
     datasets: [
       {
@@ -22,19 +25,52 @@ const DiagrammeCirculaire = (props) => {
       },
     ],
   };
+  console.log('Datas :', datas);
+  console.log('Couleurs :', couleurs);
+  console.log('Labels :', labelsProps);
+  console.log('Titre :', titre);
+  console.log('TypeDiagramme :', typeDiagramme);
+  console.log('ClickCallback :', clickCallback);
+  console.log('Id :', idProps);
+  console.log('BooleanIsInteractive :', booleanIsInteractive);
+  console.log('Taille :', taille);
   const config = {
     type: typeDiagramme,
-    data: data,
-    showTooltips: booleanIsInteractive,
+    data: datas,
+    options: { responsive: true, showTooltips: booleanIsInteractive },
   };
-  const myChart = new Chart(document.getElementById(idProps), config);
-  myChart.onClick = clickCallback;
+
+  const canvas = <canvas id={`my-canvas-${idProps}`} width={taille} height={taille}></canvas>;
+  const ctx = document.getElementById(`my-canvas-${idProps}`);
 
   return (
-    <div className={'diagramme-circulaire' + idProps}>
-      <canvas id={idProps} width={taille} height={taille}></canvas>
+    <div id={'diagramme-circulaire' + idProps} className={'diagramme' + idProps}>
+      {useEffect(() => {
+        console.log('zboob', !myChart);
+        if (!myChart) {
+          console.log('Je vais creer le chart');
+          myChart = new Chart(ctx, config);
+          myChart.onClick = clickCallback;
+        } else {
+          console.log('je vais update le chart');
+          myChart.update();
+        }
+      }, [config])}
+      {canvas}
     </div>
   );
+};
+
+DiagrammeCirculaire.propTypes = {
+  datas: PropTypes.arrayOf(PropTypes.number).isRequired,
+  couleurs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+  titre: PropTypes.string.isRequired,
+  typeDiagramme: PropTypes.string.isRequired,
+  clickCallback: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  booleanIsInteractive: PropTypes.bool.isRequired,
+  taille: PropTypes.number.isRequired,
 };
 
 export default DiagrammeCirculaire;
--- src/components/PourcentEtuParDifficulte/PourcentEtuParDifficulte.js
+++ src/components/PourcentEtuParDifficulte/PourcentEtuParDifficulte.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-
+import DiagrammeCirculaire from '../DiagrammeCirculaire/DiagrammeCirculaire'; //TODO ALIAS SUR LE COMPONENT
 // import redux/store
 import { useSelector } from 'react-redux';
 import { getExercices } from '@stores/Exercices/exercicesSlice';
@@ -21,7 +21,54 @@ const PourcentEtuParDifficulte = () => {
     },
     { total: 0 },
   );
-  return <div>{JSON.stringify(difficulteDico)}</div>; /** TODO: Affichage graphique et stylisé */
+  /*Création d'un tableau de couleurs pour déssiner le pie chart*/
+  const COLORS = [
+    'rgb(252, 232, 232)',
+    'rgb(247, 186, 186)',
+    'rgb(242, 140, 140)',
+    'rgb(237, 94, 94)',
+    'rgb(232, 48, 48)',
+    'rgb(206, 23, 23)',
+    'rgb(178, 18, 18)',
+    'rgb(115, 13, 13)',
+    'rgb(69, 8, 8)',
+    'rgb(23, 3, 3)',
+  ];
+  let couleurAEnvoyer = [];
+  let labelsAEnvoyer = [];
+  let dataAEnvoyer = [];
+
+  /* Boucler à travers le dico d'exo et pousser les valeurs dans le tableau. */
+  for (let difficulte in difficulteDico) {
+    if (difficulte != 'total') {
+      dataAEnvoyer.push(difficulteDico[difficulte]);
+      couleurAEnvoyer.push(COLORS[difficulte - 1]);
+      labelsAEnvoyer.push('Difficulté : ' + difficulte);
+    }
+  }
+  //Object.keys(difficulteDico).forEach(function (key) {}
+  let titreAEnvoyer = "Pourcentage d'étudiants par difficulté";
+  let typeDiagrammeAEnvoyer = 'pie';
+  let clickCallbackAEnvoyer = function () {
+    console.log('prout');
+  };
+  let idAEnvoyer = 'pourcentEtuParDifficulte';
+  let booleanIsInteractiveAEnvoyer = true;
+  let tailleAEnvoyer = 400;
+  return (
+    <DiagrammeCirculaire
+      datas={dataAEnvoyer}
+      taille={tailleAEnvoyer}
+      typeDiagramme={typeDiagrammeAEnvoyer}
+      titre={titreAEnvoyer}
+      couleurs={couleurAEnvoyer}
+      clickCallback={clickCallbackAEnvoyer}
+      booleanIsInteractive={booleanIsInteractiveAEnvoyer}
+      id={idAEnvoyer}
+      labels={labelsAEnvoyer}
+    />
+  );
+  //return <div>{JSON.stringify(difficulteDico)}</div>; /** TODO: Affichage graphique et stylisé */
 };
 
 export default PourcentEtuParDifficulte;
