0a3cec37d1342eedeae20f93dea24efe206b2fe5
JediSim <simpiccc@gmail.com>
2022-04-12 11:21:01 +0200

    feat: tableau des exercices fini par étudiant

--- src/components/TableEtudiantExerciceComponent/TableEtudiantExerciceComponent.js
+++ src/components/TableEtudiantExerciceComponent/TableEtudiantExerciceComponent.js
@@ -8,6 +8,7 @@ import {
   TableCell,
   TableBody,
   Table,
+  TablePagination,
 } from '@mui/material';
 
 // import redux/store
@@ -27,31 +28,37 @@ const TableEtudiantExerciceComponent = () => {
   }, {});
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Étudiant</TableCell>
-            <TableCell>Exercices</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.entries(etudiantTrie).map(([idEtu, exercices]) => (
-            <TableRow key={idEtu} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                {idEtu}
-              </TableCell>
-              <TableCell>
-                {exercices.map((exercice, key) => (
-                  <Box key={key}>{exercice.nomExo}</Box>
-                ))}
-              </TableCell>
+    <Paper style={{ width: '100%' }}>
+      <TableContainer>
+        <Table stickyHeader aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Étudiant</TableCell>
+              <TableCell>Exercices</TableCell>
+
+              {/* {exercices.map((exercice, key) => (
+              <TableCell key={key + 'title'}>{'Exercices' + key}</TableCell>
+            ))} */}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      {console.log(etudiantTrie)}
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {Object.entries(etudiantTrie).map(([idEtu, exercices]) => (
+              <TableRow key={idEtu} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {idEtu}
+                </TableCell>
+                {exercices.map((exercice, key) => (
+                  <TableCell key={key} style={{ minWidth: 10 }}>
+                    {exercice.nomExo}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {console.log(etudiantTrie)}
+      </TableContainer>
+    </Paper>
   );
 };
 
