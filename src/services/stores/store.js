e8bd9ae0255444737ab0fd7d49888e404ea2a702
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-05-17 14:39:19 +0200

    fix:

--- src/services/stores/store.js
+++ src/services/stores/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import exercicesReducer from '@stores/Exercices/exercicesSlice';
-
+/* Création d'une tranche du magasin Redux. */
+import sessionReducer from '@stores/Session/sessionSlice';
 export default configureStore({
   reducer: {
     exercices: exercicesReducer,
+    sessions: sessionReducer,
   },
 });
