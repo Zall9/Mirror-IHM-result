30112a339b8fbea1233985158278210d2d01ed75
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-05-18 18:22:43 +0200

    Refactor: Carte de l'étudiant affiche correctement le nom

--- src/pages/ResultatEtudiant/ResultatEtudiant.js
+++ src/pages/ResultatEtudiant/ResultatEtudiant.js
@@ -3,13 +3,15 @@ import Navigation from '@components/NavigationBar/NavigationBar';
 import { Box } from '@mui/material';
 import ResultatCompletEtudiant from '@components/ResultatCompletEtudiant/ResultatCompletEtudiant';
 import { useParams } from 'react-router-dom';
-import etudiantParser from '../../components/Utilitaires/Etudiant/etudiantParser';
+import { etudiantParser } from '../../components/Utilitaires/Etudiant/etudiantParser';
 const ResultatEtudiant = () => {
+  var idEtu = useParams().etu;
+  const etudiant = etudiantParser(idEtu);
   return (
     <Box>
       <Navigation />
-      <h2 align="center"> Carte de l&apos;étudiant : {useParams().etu}</h2>
-      <ResultatCompletEtudiant idEtu={useParams().etu} />
+      <h2 align="center"> Carte de l&apos;étudiant : {etudiant}</h2>
+      <ResultatCompletEtudiant idEtu={idEtu} />
     </Box>
   );
 };
