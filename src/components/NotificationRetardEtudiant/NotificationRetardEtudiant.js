fcae3717e2e50a930b4a74ab8459770e9f98aeec
JediSim <simpiccc@gmail.com>
2022-05-18 10:38:55 +0200

    doc: ajout des commentaires

--- src/components/NotificationRetardEtudiant/NotificationRetardEtudiant.js
+++ src/components/NotificationRetardEtudiant/NotificationRetardEtudiant.js
@@ -6,14 +6,20 @@ import { getExercices } from '@stores/Exercices/exercicesSlice';
 import PropTypes from 'prop-types';
 import { recupereExerciceCourantEtudiant } from '../Utilitaires/Exercices';
 
+/**
+ *  Composant qui s'affiche si l'etudiant est en retard
+ * @param {Json} props contient uniquement l'id de l'etudiant
+ * @returns Si l'etudiant a passé plus de 2 fois le temps moyen de l'exo,
+ * alors affiche une icon de retard
+ */
 const NotificationRetardEtudiant = (props) => {
   const idEtudiant = props.idEtudiant;
   const exercices = useSelector(getExercices);
   const exerciceCourant = recupereExerciceCourantEtudiant(exercices, idEtudiant);
+
+  // useState utiliser pour réafficher le composant régulièrement
   const [time, setTime] = React.useState(Date.now());
-
   let interval;
-
   React.useEffect(() => {
     interval = setInterval(() => setTime(Date.now()), 1000);
     return () => {
@@ -25,6 +31,7 @@ const NotificationRetardEtudiant = (props) => {
 
   var retard = differenceTemps > exerciceCourant.tempsMoyen * 2 * 60000;
 
+  // affiche AccessAlarmIcon si l'etudiant à passé plus de  fois le temps moyen
   return <Box>{retard ? <AccessAlarmIcon /> : null}</Box>;
 };
 
