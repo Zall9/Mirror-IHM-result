ac1c2d3f0693b034fdaafdc429712814e6895e93
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-06-07 11:59:53 +0200

    feat: ComposantResultatsGlobaux: couleurs + colonnes ok

--- src/components/ComposantResultatsGlobaux/ComposantResultatsGlobaux.js
+++ src/components/ComposantResultatsGlobaux/ComposantResultatsGlobaux.js
@@ -1,13 +1,14 @@
 import React, { useRef, useState } from 'react';
+import clsx from 'clsx';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { getExercices } from '@stores/Exercices/exercicesSlice';
 import { Box } from '@mui/system';
-import { List, ListItem, ListItemText } from '@mui/material';
 
 const HoveredItem = (props) => {
   let exo = props._getExo();
+  console.log(exo);
   return (
     <div>
       {exo && (
@@ -27,10 +28,9 @@ const ComposantResultatsGlobaux = () => {
 
   //Initialisations
   const exercices = useSelector(getExercices);
-  console.log(exercices);
   let tabEtu = [];
   let tabExo = [];
-
+  let tabIdExo = [];
   for (const exo of exercices) {
     if (!tabEtu.includes(exo.idEtu)) {
       tabEtu.push(exo.idEtu);
@@ -38,89 +38,115 @@ const ComposantResultatsGlobaux = () => {
     if (!tabExo.includes(exo.nomExo)) {
       tabExo.push(exo.nomExo);
     }
-  }
-  function getExercicese(listeExo, idExo, idEtu) {
-    for (const exo of listeExo) {
-      if (exo.idExo === idExo && exo.idEtu === idEtu) {
-        return exo.tentatives[exo.tentatives.length - 1];
-      }
+    if (!tabIdExo.includes(exo.idExo)) {
+      tabIdExo.push(exo.idExo);
     }
   }
-  // let columns = [
-  //   { field: 'idEtu', headerName: 'id etudiant', width: 180 },
-  //   {
-  //     field: 'exos',
-  //     headerName: 'exos en cours',
-  //     width: exercices.length,
-  //     renderCell: (params) => {
-  //       return (
-  //         <List disablePadding={true} sx={{ display: 'flex' }}>
-  //           {params.value.map((exo) => (
-  //             <ListItemText
-  //               onMouseOver={() => _setExo(exo)}
-  //               onMouseOut={() => _setExo(null)}
-  //               key={exo.id}
-  //             >
-  //               {exo.nomExo}
-  //             </ListItemText>
-  //           ))}
-  //         </List>
-  //       );
-  //     },
-  //   },
-  // ];
-  let columns = [{ field: 'idEtu', headerName: 'id etudiant', width: 180 }];
+  let columns = [
+    {
+      field: 'idEtu',
+      headerName: 'id etudiant',
+      width: 180,
+      cellClassName: 'super-app-theme--cell',
+    },
+  ];
   for (const exo of exercices) {
     if (!columns.find((col) => col.field === exo.idExo)) {
       columns.push({
         field: exo.idExo,
         headerName: exo.idExo,
-        renderCell: (params) => {
-          return (
-            <Box
-              sx={{ margin: 0, padding: 0, backgroundColor: '#ff000011', width: 100, height: 100 }}
-            ></Box>
-          );
+        cellClassName: (params) => {
+          if (params.value === true) {
+            return '';
+          }
+          return clsx('super-app', {
+            negative: params.value <= 3,
+            normal: params.value <= 6 && params.value > 3,
+            positive: params.value > 6,
+          });
         },
       });
     }
   }
-  let rows_etu = [];
+
   let exoEtu = [];
   for (const etu of tabEtu) {
     exoEtu = exercices.filter((exo) => exo.idEtu == etu);
   }
-  //genre le dico des exercices
-  let dicoExo = {};
-  for (const exo of exercices) {
-    dicoExo[exo.idExo] = exo;
-  }
-  console.log('dico', dicoExo);
+  let exoEtus = [];
   for (const etu of tabEtu) {
-    rows_etu.push({
+    exoEtus.push({
       id: etu,
       idEtu: etu,
       exos: exercices.filter((exo) => exo.idEtu == etu),
     });
   }
+  let rows_etu = [];
+  for (const etu of exoEtus) {
+    let row_etu = {
+      id: etu.idEtu,
+      idEtu: etu.idEtu,
+    };
+    for (const exo of etu.exos) {
+      row_etu[exo.idExo] = exo.tentatives.length + 1;
+    }
+    rows_etu.push(row_etu);
+  }
 
+  const choisiCouleurSelonNbTentativesEtDifficulte = (nbTentatives, difficulte) => {
+    const coulNbTentative = ['#097504', '#F96D0C', '#D10D04'];
+    if (nbTentatives <= Math.ceil(difficulte / 2) + 1) {
+      return coulNbTentative[0];
+    } else if (nbTentatives <= difficulte + 2) {
+      return coulNbTentative[1];
+    } else {
+      return coulNbTentative[2];
+    }
+  };
   return (
     <>
-      <HoveredItem _setExo={_setExo} _getExo={_getExo} />
-      <DataGrid
-        rows={rows_etu}
-        columns={columns}
-        loading={rows_etu.length === 2}
-        autoHeight={true}
-        autoWidth={true}
-        headerHeight={36}
-        density={'compact'}
-        initialState={{
-          sorting: {
-            sortModel: [{ field: 'idEtu', sort: 'desc' }],
+      <Box
+        sx={{
+          height: 300,
+          width: 1,
+          '& .super-app-theme--cell': {
+            backgroundColor: '#f5f6fa',
+            color: '#1a3e72',
+            fontWeight: '600',
+          },
+          '& .super-app.negative': {
+            backgroundColor: '#097504', //vert
+            color: '#1a3e72',
+            fontWeight: '600',
+          },
+          '& .super-app.positive': {
+            backgroundColor: '#D10D04', //orange
+            color: '#1a3e72',
+            fontWeight: '600',
+          },
+          '& .super-app.normal': {
+            backgroundColor: '#F96D0C', //rouge
+            color: '#1a3e72',
+            fontWeight: '600',
           },
         }}
-      />
+      >
+        <HoveredItem _setExo={_setExo} _getExo={_getExo} />
+        <DataGrid
+          rows={rows_etu}
+          columns={columns}
+          loading={rows_etu.length === 2}
+          autoHeight={true}
+          autoWidth={true}
+          headerHeight={36}
+          density={'compact'}
+          initialState={{
+            sorting: {
+              sortModel: [{ field: 'idEtu', sort: 'desc' }],
+            },
+          }}
+        />
+      </Box>
     </>
   );
 };
