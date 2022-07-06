d9b0f21f2c40a29ee478adf7b551aa4ff5756d5e
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-07-06 14:56:43 +0200

    doc: docstring pour les utilitaires

--- src/components/ComposantResultatsGlobaux/utils/dateParser.js
+++ src/components/ComposantResultatsGlobaux/utils/dateParser.js
@@ -1,3 +1,10 @@
+/**
+ * prend une date comme paramètre, la convertit en chaîne, la divise en un tableau, divise le
+ * deuxième élément du tableau en un autre tableau, puis renvoie le premier élément du deuxième tableau
+ * avec un 'h' entre les deux et le deuxième élément du deuxième tableau
+ * @param date - la date que vous voulez analyser
+ * @returns La date est renvoyée dans un format différent.
+ */
 export const dateParser = (date) => {
   let newDate = new Date(date).toLocaleDateString('fr-FR', {
     hour: 'numeric',
@@ -9,6 +16,12 @@ export const dateParser = (date) => {
   return newDateArray3;
 };
 
+/**
+ * prend deux dates et renvoie la différence entre elles en minutes et secondes
+ * @param date1 - La date de début
+ * @param date2 - La date actuelle
+ * @returns Une chaîne qui représente la différence de temps entre deux dates.
+ */
 export const calculateTime = (date1, date2) => {
   const DATE1 = _dateToTimestamp(date1);
   const DATE2 = _dateToTimestamp(date2);
@@ -17,6 +30,11 @@ export const calculateTime = (date1, date2) => {
   const second = diff % 60;
   return minute + 'm' + (second < 10 ? '0' : '') + second + 's';
 };
+/**
+ * Convertir une date en timestamp.
+ * @param date - Date à convertir en horodatage.
+ * @returns l'horodatage de la date transmise.
+ */
 const _dateToTimestamp = (date) => {
   return new Date(date).getTime();
 };
